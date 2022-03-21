96ad910c09b5a53a0f1d93cb288b6a43fb28397e
unknown <nimishsanghvi31@gmail.com>
2022-03-21 12:59:29 +0530

    Initialized our Backend and Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App=()=> //it can return only one div
+    // <div>
+    //   <div className="App">Hello World! {hello}</div>
+    //   <div className="App">Hello World! {hello1}</div>
+    // </div>
+    <>
+      <div className="App">Hello World!</div>
+    </>
+
+
+export default App;
